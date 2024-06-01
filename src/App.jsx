1359eb4771e88e57124a0cbe81afef74d005c54e
BrandonReyes0609 <rey22992@uvg.edu.gt>
2024-06-01 00:06:57 -0600

    1

--- src/App.jsx
+++ src/App.jsx
@@ -17,17 +17,17 @@ function App() {
           <div class="col p-3 bg-primary text-white">Brandon Javier Reyes Morales</div>
           <div class="col p-3 bg-primary text-white"></div>
           <div class="col p-3 bg-primary text-white"></div>
+        </div>
         <div class="row">
           <div class="col p-3 bg-dark text-white">Estudiante Ingenieria en Ciencias de la Computación y Tecnologías de la información</div>
           <div class="col p-3 bg-primary text-white"></div>
           <div class="col p-3 bg-primary text-white"></div>
 
-          </div>
+        </div>
         <div class="row">
           <div class="col p-3 bg-primary text-white">Univerdad del Valle de Guatemala</div>
           <div class="col p-3 bg-primary text-white"></div>
           <div class="col p-3 bg-primary text-white"></div>
-
         </div>
     </div> 
 
