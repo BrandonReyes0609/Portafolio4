43ee9336c4934452fcbff5a7ffda2b0ff039f94c
BrandonReyes0609 <rey22992@uvg.edu.gt>
2024-06-01 00:18:57 -0600

    fix container

--- src/App.jsx
+++ src/App.jsx
@@ -9,32 +9,31 @@ function App() {
   return (
     <>
       <div class=".container-fluid">
-      <div class="row">
-        <div class="col ">
-          <div class="row">
-            <div class="col p-3 bg-dark text-white">Brandon Javier Reyes Morales</div>
-            <div class="col p-3 bg-dark text-white"></div>
-            <div class="col p-3 bg-dark text-white"></div>
-          </div>
-          <div class="row">
-            <div class="col p-3 bg-dark text-white">Estudiante Ingenieria en Ciencias de la Computación y Tecnologías de la información</div>
-            <div class="col p-3 bg-dark text-white"></div>
-            <div class="col p-3 bg-dark text-white"></div>
+        <div class="row">
+          <div class="col ">
+            <div class="row">
+              <div class="col p-3 bg-dark text-white">Brandon Javier Reyes Morales</div>
+              <div class="col p-3 bg-dark text-white"></div>
+              <div class="col p-3 bg-dark text-white"></div>
+            </div>
+            <div class="row">
+              <div class="col p-3 bg-dark text-white">Estudiante Ingenieria en Ciencias de la Computación y Tecnologías de la información</div>
+              <div class="col p-3 bg-dark text-white"></div>
+              <div class="col p-3 bg-dark text-white"></div>
 
+            </div>
+            <div class="row">
+              <div class="col p-3 bg-dark text-white">Univerdad del Valle de Guatemala</div>
+              <div class="col p-3 bg-dark text-white"></div>
+              <div class="col p-3 bg-dark text-white"></div>
+            </div>
           </div>
-          <div class="row">
-            <div class="col p-3 bg-dark text-white">Univerdad del Valle de Guatemala</div>
-            <div class="col p-3 bg-dark text-white"></div>
-            <div class="col p-3 bg-dark text-white"></div>
+          <div class="col">
+            imagen brandon
           </div>
-        
-        <div class="col">
-          imagen brandon
         </div>
-      </div>
+      </div> 
 
-      </div> 
-      </div> 
 
     </>
   )
