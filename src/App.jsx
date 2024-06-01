8a635f293d44be864d950d32a30922c4ce35e5cb
BrandonReyes0609 <rey22992@uvg.edu.gt>
2024-06-01 00:42:13 -0600

    3

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,21 @@ import fotoperfil from './assets/foto_perfil.jpeg'; // Importar la imagen
 function App() {
   return (
     <>
-      <div className="container-fluid col p-3 bg-dark text-white">
+      <div className="container-fluid col  bg-dark text-white">
         <div className="row">
           <div className="col-md-8">
             <div className="row">
-              <div className="col p-3 bg-dark text-white">
+              <div className="col  bg-dark text-white">
                 <p className="text-start">Brandon Javier Reyes Morales</p>
               </div>
             </div>
             <div className="row">
-              <div className="col p-3 bg-dark text-white">
+              <div className="col  bg-dark text-white">
                 <p className="text-start">Estudiante Ingenieria en Ciencias de la Computación y Tecnologías de la información</p>
               </div>
             </div>
             <div className="row">
-              <div className="col p-3 bg-dark text-white">
+              <div className="col  bg-dark text-white">
                 <p className="text-start">Universidad del Valle de Guatemala</p>
               </div>
             </div>
