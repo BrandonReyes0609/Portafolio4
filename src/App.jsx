12df00a1aa3be20fc68846e93f0ab1cabf4109de
BrandonReyes0609 <rey22992@uvg.edu.gt>
2024-06-01 00:36:26 -0600

    fix plromes 1

--- src/App.jsx
+++ src/App.jsx
@@ -10,23 +10,23 @@ function App() {
   return (
     <>
       <div class=".container-fluid col p-3 bg-dark text-white">
-        <p class="text-start">
+        
           <div class="row">
             <div class="col ">
               <div class="row">
-                <div class="col p-3 bg-dark text-white">Brandon Javier Reyes Morales</div>
+                <div class="col p-3 bg-dark text-white"><p class="text-start"> Brandon Javier Reyes Morales</p></div>
                 <div class="col p-3 bg-dark text-white"></div>
 
               </div>
               <div class="row">
-                <div class="col p-3 bg-dark text-white">Estudiante Ingenieria en Ciencias de la Computación y Tecnologías de la información</div>
+                <div class="col p-3 bg-dark text-white"><p class="text-start">Estudiante Ingenieria en Ciencias de la Computación y Tecnologías de la información</p></div>
                 <div class="col p-3 bg-dark text-white"></div>
 
 
 
               </div>
               <div class="row">
-                <div class="col p-3 bg-dark text-white">Univerdad del Valle de Guatemala</div>
+                <div class="col p-3 bg-dark text-white"><p class="text-start"> Univerdad del Valle de Guatemala</p></div>
                 <div class="col p-3 bg-dark text-white"></div>
 
               </div>
@@ -35,7 +35,6 @@ function App() {
               <img src={fotoperfil} alt="fotoperfil"style={{ width: '50%', height: 'auto' }} class="rounded-circle" />
             </div>
           </div>
-        </p>
       </div> 
 
 
