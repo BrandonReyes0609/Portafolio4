32e63137f1973f489d5b9ac32de454d9788aa081
BrandonReyes0609 <rey22992@uvg.edu.gt>
2024-06-01 00:32:48 -0600

    change text postition

--- src/App.jsx
+++ src/App.jsx
@@ -10,29 +10,31 @@ function App() {
   return (
     <>
       <div class=".container-fluid col p-3 bg-dark text-white">
-        <div class="row">
-          <div class="col ">
-            <div class="row">
-              <div class="col p-3 bg-dark text-white">Brandon Javier Reyes Morales</div>
-              <div class="col p-3 bg-dark text-white"></div>
+        <p class="text-start">
+          <div class="row">
+            <div class="col ">
+              <div class="row">
+                <div class="col p-3 bg-dark text-white">Brandon Javier Reyes Morales</div>
+                <div class="col p-3 bg-dark text-white"></div>
 
+              </div>
+              <div class="row">
+                <div class="col p-3 bg-dark text-white">Estudiante Ingenieria en Ciencias de la Computación y Tecnologías de la información</div>
+
+
+
+              </div>
+              <div class="row">
+                <div class="col p-3 bg-dark text-white">Univerdad del Valle de Guatemala</div>
+                <div class="col p-3 bg-dark text-white"></div>
+
+              </div>
             </div>
-            <div class="row">
-              <div class="col p-3 bg-dark text-white">Estudiante Ingenieria en Ciencias de la Computación y Tecnologías de la información</div>
-
-
-
-            </div>
-            <div class="row">
-              <div class="col p-3 bg-dark text-white">Univerdad del Valle de Guatemala</div>
-              <div class="col p-3 bg-dark text-white"></div>
-
+            <div class="col col p-3 bg-dark text-white">
+              <img src={fotoperfil} alt="fotoperfil"style={{ width: '50%', height: 'auto' }}  />
             </div>
           </div>
-          <div class="col col p-3 bg-dark text-white">
-            <img src={fotoperfil} alt="fotoperfil"style={{ width: '50%', height: 'auto' }}  />
-          </div>
-        </div>
+        </p>
       </div> 
 
 
